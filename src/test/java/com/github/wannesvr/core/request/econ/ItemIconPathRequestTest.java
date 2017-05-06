49fdb0ae19fade8339615c5778a26d6fdea13b61
Wannes Van Regenmortel <wannesvr@hotmail.com>
2017-05-07 00:13:55 +0200

    completed test

--- src/test/java/com/github/wannesvr/core/request/econ/ItemIconPathRequestTest.java
+++ src/test/java/com/github/wannesvr/core/request/econ/ItemIconPathRequestTest.java
@@ -1,8 +1,13 @@
 package com.github.wannesvr.core.request.econ;
 
+import com.github.wannesvr.base.BaseTest;
+import com.github.wannesvr.testhelpers.UrlParamHelper;
 import org.junit.Test;
 
-public class ItemIconPathRequestTest {
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ItemIconPathRequestTest extends BaseTest {
     @Test
     public void testGetItemIconPathRequest() {
         ItemIconPathRequest itemIconPathRequest = new ItemIconPathRequest
@@ -11,6 +16,9 @@ public class ItemIconPathRequestTest {
                 .language("en")
                 .build();
 
+        UrlParamHelper helper = new UrlParamHelper(itemIconPathRequest.getRequest());
 
+        assertThat(helper.getParameterValueByKey("itemizedonly"), is("0"));
+        assertThat(helper.getParameterValueByKey("language"), is("en"));
     }
 }
